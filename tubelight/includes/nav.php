9f41f4e1de828521c503fb96f7479c9a28a894fa
unknown <sitanath.cse10@gmail.com>
2019-10-22 02:18:29 +0600

    create project

---
+++ tubelight/includes/nav.php
@@ -0,0 +1,50 @@
+
+
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+        aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
+    <span class="nav-brand h3 text-light ">Bangamata Hall</span>
+        <!-- Links -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+            </li>
+         
+            
+            <li class="nav-item">
+                <a class="nav-link" href="p.php">Provost</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="create.php">Application Form</a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="custom_query.php">Custom Query</a>
+            </li>
+
+
+
+        </ul>
+        <!-- Links -->
+
+   
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
